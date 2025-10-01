622169907b6c04bf4c22fa7b9d699bc6093513e0
Jérémy Gendre <jeremy@atipik.ch>
2025-10-01 16:15:21 +0200

    feat: add tests for EntityValidator and transactional inner context

--- src/example.test.ts
+++ src/example.test.ts
@@ -32,12 +32,17 @@ class Notification {
   @ManyToOne(() => User)
   recipient: User;
 
-  constructor(user: User) {
+  @Property()
+  type: string;
+
+  constructor(user: User, type = 'test') {
     this.recipient = user;
+    this.type = type;
   }
 }
 
 let orm: MikroORM;
+let notification: Notification;
 
 beforeAll(async () => {
   orm = await MikroORM.init({
@@ -53,63 +58,61 @@ afterAll(async () => {
   await orm.close(true);
 });
 
-test('transactional fail, because non-populated relation on a fetch entity', async () => {
+beforeEach(async () => {
+  // remove all entities
+  await orm.em.nativeDelete(Notification, {});
+  await orm.em.nativeDelete(User, {});
+  orm.em.clear();
+
+  // create new entities
   const user1 = new User('Bar', 'Foo');
   await orm.em.persistAndFlush(user1);
   orm.em.clear();
 
-  const notification = new Notification(user1);
+  notification = new Notification(user1);
   await orm.em.persistAndFlush(notification);
   orm.em.clear();
-
-  await orm.em.findOneOrFail(Notification, { id: notification.id });
-  await orm.em.transactional(async () => {
-    // ...
-  })
 });
 
-test('transactional success, because of clear:true option', async () => {
-  const user1 = new User('Bar', 'Foo');
-  await orm.em.persistAndFlush(user1);
-  orm.em.clear();
-
-  const notification = new Notification(user1);
-  await orm.em.persistAndFlush(notification);
-  orm.em.clear();
-
-  await orm.em.findOneOrFail(Notification, { id: notification.id });
-  await orm.em.transactional(async () => {
-    // ...
-}, { clear: true })
-});
-
-test('transactional success, thanks to em.clear()', async () => {
-  const user1 = new User('Bar', 'Foo');
-  await orm.em.persistAndFlush(user1);
-  orm.em.clear();
-
-  const notification = new Notification(user1);
-  await orm.em.persistAndFlush(notification);
-  orm.em.clear();
+describe('transactional', () => {
+  test('fail, because non-populated relation on a fetch entity', async () => {
+    await orm.em.findOneOrFail(Notification, { id: notification.id });
+    await orm.em.transactional(async () => {})
+  });
   
-  await orm.em.findOneOrFail(Notification, { id: notification.id });
-  orm.em.clear();
-  await orm.em.transactional(async () => {
-    // ...
-  })
+  test('success, because of clear:true option', async () => {
+    await orm.em.findOneOrFail(Notification, { id: notification.id });
+    await orm.em.transactional(async () => {}, { clear: true })
+  });
+  
+  test('success, because em.clear() before', async () => {
+    await orm.em.findOneOrFail(Notification, { id: notification.id });
+    orm.em.clear();
+    await orm.em.transactional(async () => {})
+  });
+  
+  test('success, because populated relation in context', async () => {
+    await orm.em.findOneOrFail(Notification, { id: notification.id }, { populate: ['recipient'] });
+    await orm.em.transactional(async () => {})
+  });
+  
+  test('fail, with fetch in inner context', async () => {
+    await orm.em.transactional(async (em) => {
+      await em.findOneOrFail(Notification, { id: notification.id });
+    }, { clear: true });
+  });
 });
 
-test('transactional success, because no fetched entity in context', async () => {
-  const user1 = new User('Bar', 'Foo');
-  await orm.em.persistAndFlush(user1);
-  orm.em.clear();
 
-  const notification = new Notification(user1);
-  await orm.em.persistAndFlush(notification);
-  orm.em.clear();
-
-  await orm.em.findOneOrFail(Notification, { id: notification.id }, { populate: ['recipient'] });
-  await orm.em.transactional(async () => {
-    // ...
-  })
+describe('EntityValidator', () => {
+  test('validate fails, because relation not populated', async () => {
+    const fecthedNotification = await orm.em.findOneOrFail(Notification, { id: notification.id });
+    const userMetadata = orm.em.getMetadata().find(User);
+    orm.em.getValidator().validate(fecthedNotification.recipient, fecthedNotification.recipient, userMetadata!);
+  });
+  test('validate succeed, because relation populated', async () => {
+    const fecthedNotification = await orm.em.findOneOrFail(Notification, { id: notification.id }, { populate: ['recipient'] });
+    const userMetadata = orm.em.getMetadata().find(User);
+    orm.em.getValidator().validate(fecthedNotification.recipient, fecthedNotification.recipient, userMetadata!);
+  });
 });
